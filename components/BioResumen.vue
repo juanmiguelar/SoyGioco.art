f45b44306ed11edc67d45f1511f657c450871522
iamjuandev <juanmiguelar09@gmail.com>
2025-08-21 20:19:04 -0600

    working on bio

--- components/BioResumen.vue
+++ components/BioResumen.vue
@@ -1,10 +1,44 @@
 <template>
-  <v-container>
-    <h2 class="text-h5 font-weight-medium mb-2">Sobre mí</h2>
-    <p>
-      Gioconda Cerdas Agüero es educadora artística con más de ocho años de
-      experiencia diseñando planeamientos, talleres y eventos que inspiran a
-      niños, jóvenes y adultos a expresarse por medio de las artes plásticas.
-    </p>
+  <v-container class="py-6">
+    <v-row align="center" justify="center">
+      <!-- Image Column -->
+      <v-col 
+        cols="12" 
+        sm="4" 
+        md="3"
+        class="text-center"
+      >
+        <img
+          src="/img/gioconda-cerdas.png"
+          alt="Gioconda Cerdas Agüero"
+          class="rounded-circle elevation-3"
+          style="
+            max-width: 200px; 
+            width: 100%; 
+            height: auto;
+            object-fit: cover;
+          "
+        />
+      </v-col>
+      
+      <!-- Content Column -->
+      <v-col 
+        cols="12" 
+        sm="8" 
+        md="9"
+      >
+        <div class="pl-sm-4">
+          <h2 class="text-h5 text-md-h4 font-weight-medium mb-3 text-center text-sm-left">
+            Sobre mí
+          </h2>
+          
+          <p class="text-body-1 text-justify mb-0">
+            Gioconda Cerdas Agüero es educadora artística con más de ocho años de
+            experiencia diseñando planeamientos, talleres y eventos que inspiran a
+            niños, jóvenes y adultos a expresarse por medio de las artes plásticas.
+          </p>
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
