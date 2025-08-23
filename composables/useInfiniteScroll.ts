3e4a7a7652097f824918bdc6849c1040a67c704c
Juan Miguel  Arias Mejias <juanmiguelar09@gmail.com>
2025-08-22 22:49:34 -0600

    refactor: use native intersection observer

--- composables/useInfiniteScroll.ts
+++ composables/useInfiniteScroll.ts
@@ -1,9 +1,24 @@
+import { onMounted, onBeforeUnmount, ref } from 'vue'
+
 export const useInfiniteScroll = (onLoad: () => void) => {
   const target = ref<HTMLElement | null>(null)
-  useIntersectionObserver(target, ([entry]) => {
-    if (entry.isIntersecting) {
-      onLoad()
-    }
+  let observer: IntersectionObserver | null = null
+
+  const observe = () => {
+    if (!target.value) return
+    observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        onLoad()
+      }
+    })
+    observer.observe(target.value)
+  }
+
+  onMounted(observe)
+
+  onBeforeUnmount(() => {
+    observer?.disconnect()
   })
+
   return { target }
 }
