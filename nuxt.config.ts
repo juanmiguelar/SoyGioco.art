e0f583e6b3d39f6920ca0e818bb85cfb968e8cd3
iamjuandev <juanmiguelar09@gmail.com>
2025-08-25 23:07:12 -0600

    strapi init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,33 @@
-import vuetify from 'vite-plugin-vuetify'
+import vuetify from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['nuxt-simple-sitemap'],
+  modules: ["nuxt-simple-sitemap", "@nuxtjs/strapi"],
   sitemap: {
-    urls: ['https://soygioco.art']
+    urls: ["https://soygioco.art"],
   },
   app: {
     head: {
-      htmlAttrs: { lang: 'es-CR' },
-      titleTemplate: '%s | Soy Gioco Arte',
+      htmlAttrs: { lang: "es-CR" },
+      titleTemplate: "%s | Soy Gioco Arte",
       meta: [
-        { name: 'twitter:card', content: 'summary_large_image' },
-        { property: 'og:type', content: 'website' },
-        { property: 'og:site_name', content: 'Soy Gioco Arte' },
-        { name: 'author', content: 'Soy Gioco Arte' }
+        { name: "twitter:card", content: "summary_large_image" },
+        { property: "og:type", content: "website" },
+        { property: "og:site_name", content: "Soy Gioco Arte" },
+        { name: "author", content: "Soy Gioco Arte" },
       ],
-      link: [
-        { rel: 'icon', type: 'image/jpeg', href: '/img/logo.jpg' }
-      ]
-    }
+      link: [{ rel: "icon", type: "image/jpeg", href: "/img/logo.jpg" }],
+    },
   },
-  css: [
-    'vuetify/styles',
-    '@mdi/font/css/materialdesignicons.min.css'
-  ],
+  css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.min.css"],
   build: {
-    transpile: ['vuetify']
+    transpile: ["vuetify"],
   },
   vite: {
     ssr: {
-      noExternal: ['vuetify']
+      noExternal: ["vuetify"],
     },
-    plugins: [vuetify()]
+    plugins: [vuetify()],
   }
-})
+});
